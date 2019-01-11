b07af968638674d43da1aed7d51a15f385c0607c
Aleksey Timashkov <atimashkov@alvioneurope.ru>
2019-01-11 09:21:13 +0300

    removed unused items

--- utlis.cpp
+++ utlis.cpp
@@ -7,26 +7,6 @@
 //
 
 #include "utils.h"
-#ifdef LOCAL_RUN
-#include <fstream>
-std::ofstream myfile;
-#endif
-
-void writeLog(const std::stringstream& ss){
-#ifdef LOCAL_RUN
-    if (!myfile.is_open())
-        myfile.open ("example.txt");
-    myfile << ss.str() <<std::endl;
-
-#endif
-}
-
-void closeLog(){
-#ifdef LOCAL_RUN
-    if (myfile.is_open())
-        myfile.close();
-#endif
-}
 
 Vec3 Vec3::None = Vec3(-1000,-1000,-1000);
 
